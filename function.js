393c39fb5e40cbd6d610b559f0ac20c0afdac7f1
Faheem <i.am.faheem.iqbal@gmail.com>
2020-05-08 16:44:26 +0500

    Changes

--- function.js
+++ function.js
@@ -5,35 +5,38 @@ jQuery(document).ready(() => {
   );
 
   jQuery(".inputbox a").on("click", () => {
-    var tempData = document.createElement("div");
-    var link = jQuery(".inputbox input")[0].value;
-    fetch(link).then((response) => {
-      response.text().then((html) => {
-        tempData.innerHTML = html;
-        try {
-          jQuery("#chapter-content").html(
-            tempData.querySelector("#chaptercontent")
-          );
-        } catch (e) {
-          console.log(e);
-        }
+    if (jQuery(".inputbox input")[0].value.length != 0) {
+      var tempData = document.createElement("div");
+      var link = jQuery(".inputbox input")[0].value;
+      fetch(link).then((response) => {
+        response.text().then((html) => {
+          tempData.innerHTML = html;
+          try {
+            jQuery("#chapter-content").html(
+              tempData.querySelector("#chaptercontent")
+            );
+          } catch (e) {
+            console.log(e);
+          }
 
-        var novelLink = getNovel(link);
-        prev_link =
-          novelLink + tempData.querySelector("#pt_prev").getAttribute("href");
-        next_link =
-          novelLink + tempData.querySelector("#pt_next").getAttribute("href");
+          var novelLink = getNovel(link);
+          prev_link =
+            novelLink + tempData.querySelector("#pt_prev").getAttribute("href");
+          next_link =
+            novelLink + tempData.querySelector("#pt_next").getAttribute("href");
+
+          jQuery("li.prev a.btn").on("click", () => {
+            localStorage["chapterLink"] = prev_link;
+          });
+          jQuery("li.next a.btn").on("click", () => {
+            localStorage["chapterLink"] = next_link;
+          });
+        });
       });
-    });
-    localStorage["chapterLink"] = link;
-    jQuery(".inputbox input")[0].value = localStorage["chapterLink"];
-  });
-
-  jQuery("li.prev a.btn").on("click", () => {
-    localStorage["chapterLink"] = prev_link;
-  });
-  jQuery("li.next a.btn").on("click", () => {
-    localStorage["chapterLink"] = next_link;
+      localStorage["chapterLink"] = jQuery(".inputbox input")[0].value;
+    } else {
+      localStorage["chapterLink"] = "";
+    }
   });
 
   getNovel = (url) => {
