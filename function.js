c05a9a504c50aa11edc7cb2f057086a1888e9636
Faheem <i.am.faheem.iqbal@gmail.com>
2020-10-27 19:50:03 +0500

    Update function.js

--- function.js
+++ function.js
@@ -1,29 +1,28 @@
 jQuery(document).ready(() => {
   var next_link, prev_link;
-  jQuery("div#content-container .section-content").before(
-    "<div class='inputbox'><input placeholder='Chapter Link.....' /><a href='#'>Get Chapter</a></div>"
+  jQuery("#content-container").before(
+    "<div class='inputbox'><input placeholder='chaplink' /><a href='#'>Get Chapter</a></div>"
   );
 
   jQuery(".inputbox a").on("click", () => {
     if (jQuery(".inputbox input")[0].value.length != 0) {
       var tempData = document.createElement("div");
       var link = jQuery(".inputbox input")[0].value;
-      fetch(link).then((response) => {
+      fetch(`https://cors-anywhere.herokuapp.com/${link}`).then((response) => {
         response.text().then((html) => {
           tempData.innerHTML = html;
+          console.log(tempData);
           try {
-            jQuery("#chapter-content").html(
-              tempData.querySelector("#chaptercontent")
-            );
+            jQuery("#chapter-content").html(tempData.querySelector("#chapter-entity").innerHTML.split("<br><br>").join("</p><p>"));
           } catch (e) {
             console.log(e);
           }
 
           var novelLink = getNovel(link);
           prev_link =
-            novelLink + tempData.querySelector("#pt_prev").getAttribute("href");
+            novelLink + tempData.querySelector(".reader-page a.prev").getAttribute("href");
           next_link =
-            novelLink + tempData.querySelector("#pt_next").getAttribute("href");
+            novelLink + tempData.querySelector(".reader-page a.next").getAttribute("href");
 
           jQuery("li.prev a.btn").on("click", () => {
             localStorage["chapterLink"] = prev_link;
@@ -40,9 +39,7 @@ jQuery(document).ready(() => {
   });
 
   getNovel = (url) => {
-    const novelLink = `https://${
-      url.replace(/^https?:\/\//, "").split("/")[0]
-    }/${url.replace(/^https?:\/\//, "").split("/")[1]}/`;
+    const novelLink = `https://m.wuxiaworld.co`;
     return novelLink;
   };
 
